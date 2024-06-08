6b76a615b4619899196ae5b44ba21fed31a29eaf
AduMano <samano.aldwindylan.07292003@gmail.com>
2024-06-08 22:18:55 +0800

    Done Exercise#4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+// arr parameter holds the first argument, which is the sets.
+// the ...target is the rest of the arguments stored in the array, this is called
+// "Spread Operator". Thanks Odin!
+
+const removeFromArray = function(arr, ...target) {
+    // Using filter, It iterates the items in arr one by one and check if they are not
+    // included in the target array, if they are not included then proceed on the iteration.
+    // if they are in the target array they wont be making inside this new array.
+    
+    return arr.filter((item) => {
+        return (!target.includes(item));
+    });
 };
 
 // Do not edit below this line
