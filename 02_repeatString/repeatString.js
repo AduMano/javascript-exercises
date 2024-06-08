dcb3f7ac0edd7bf630fd1fdb4dfc80d7d6c19ff4
AduMano <samano.aldwindylan.07292003@gmail.com>
2024-06-08 21:52:36 +0800

    Done exercise#2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(str, it) {
+    if (it < 0) {
+        return "ERROR";
+    }
+    else if (it == 0){
+        return "";
+    }
 
+    let newStr = "";
+
+    for (let i = 0; i < it; i++) {
+        newStr += str;
+    }
+
+    return newStr;
 };
 
 // Do not edit below this line
