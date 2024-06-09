6ff7bbd5aeac3e988e8734c24d521cc938a19e1b
AduMano <samano.aldwindylan.07292003@gmail.com>
2024-06-09 22:14:14 +0800

    Done Exercise#12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    // Whats need to be done:
+    // I have to compare all the objects years of birth and death
+    // and return the object of the oldest
 
+    return arr.reduce((oldestPerson, person) => {
+        let pDeath = ((person.yearOfDeath == undefined) ? new Date().getFullYear() : person.yearOfDeath);
+        let pBirth = person.yearOfBirth;
+        let opDeath = ((oldestPerson.yearOfDeath == undefined) ? new Date().getFullYear() : oldestPerson.yearOfDeath);
+        let opBirth = oldestPerson.yearOfBirth;
+
+        let pAge = pDeath - pBirth;
+        let opAge = opDeath - opBirth;
+
+        // Check whos older
+        return (pAge > opAge) ? person : oldestPerson;
+    }, arr[0]);
 };
 
 // Do not edit below this line
