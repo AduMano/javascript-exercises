63a51186aa9e7c4ded7d1af38dcb0e8e25f815da
AduMano <samano.aldwindylan.07292003@gmail.com>
2024-06-09 19:12:13 +0800

    Done Exercise#9 | I got stuck at the last test, which made me realize that numbers are considered to be part of the palindromes lol

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Use regex to grab only letters and numbers (a-zA-Z0-9)
+    // The "g" flag makes it check the whole string
+    // The [^] means "not", so it finds things that are not in the set
+    // Then I replace those that are not in the set with an empty string
 
+    let newStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let reversed = newStr.split("").reverse().join("").toLowerCase();
+
+    return (newStr === reversed);
 };
 
 // Do not edit below this line
