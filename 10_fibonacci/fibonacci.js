9ad5e0de7ec8599779f458bdaed984c8877f7622
AduMano <samano.aldwindylan.07292003@gmail.com>
2024-06-09 21:10:44 +0800

    Done Exercise#10 | Got stuck, decided to make an array instead

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num == 0) return 0;
+    else if (num < 0) return "OOPS";
 
+    let fib = [0];
+
+    // Make an array of fibonacci
+    for (let i = 0; i < num; i++) {
+        if (i == 0 || i == 1) {
+            fib.push(1);
+            continue;
+        }
+
+        fib.push(( fib[i] + fib[i-1] ));   
+    }
+
+    return fib[num];
 };
 
 // Do not edit below this line
