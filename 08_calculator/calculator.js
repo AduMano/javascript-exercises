dc2eab2bb1a50e7a30cca9895f1342aa549a39d3
AduMano <samano.aldwindylan.07292003@gmail.com>
2024-06-09 18:24:55 +0800

    Done Exercise#8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) return 0;
+	return arr.reduce((prev, curr) => { return prev + curr });
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prev, curr) => { return prev * curr });
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0 || num == 1) return 1;
+
+  let factorial = 1;
+
+  for (let i = num; i >= 1; i--) {
+    factorial *= i;
+  }
+
+  return factorial;
 };
 
 // Do not edit below this line
